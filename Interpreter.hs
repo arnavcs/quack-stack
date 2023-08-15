9d47beace05a422fc364d07604a7224a8892780f
Arnav Kumar <arnav.kumar.sri@gmail.com>
2023-08-15 10:52:43 -0600

    commited restart of project

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,32 +1,123 @@
-module Interpreter (interpretProgram
-                  , interpretExpression) where
+module Interpreter where
 
-import Lexer
-import Parser
+----------------
+-- DATA TYPES --
+----------------
 
-----------
--- DATA --
-----------
+type CleanStack 
+    = [CleanTerm]
 
-type EvaluationState = (Stack, Stack)
+type DirtyStack
+    = [DirtyTerm]
 
-------------------
--- INTERPRETING --
-------------------
+data StackZipper
+    = StackZipper CleanStack DirtyStack
+  deriving (Show, Eq)
 
-interpretProgram :: Program -> String
-interpretProgram (Program env exprs) = unlines $ interpretExpression <$> exprs <*> pure env
+type Routine 
+    = [DirtyTerm] 
 
-interpretExpression :: Stack -> Environment -> String
-interpretExpression stk = interpretExpression' ([], stk)
+data PrimOp
+    = Add
+    | Neg
+    | Not
+    | And
+    | Or
+    | Compose
+    | Push
+    | Pop
+  deriving (Show, Eq)
+
+data CleanTerm 
+    = CInt Int
+    | CChar Char
+    | CBool Bool
+    | CStack CleanStack
+    | CRoutine Routine
+    | CPrimOp PrimOp
+  deriving (Show, Eq)
+
+data DirtyTerm 
+    = DInt Int
+    | DChar Char
+    | DBool Bool
+    | DStack DirtyStack
+    | DRoutine Routine
+    | DPrimOp PrimOp
+    | DQuack
+  deriving (Show, Eq)
+
+-------------------------------
+-- EXPRESSION INTERPRETATION --
+-------------------------------
+
+applyPrimOp :: PrimOp -> StackZipper -> StackZipper
+applyPrimOp p (StackZipper cs ds)
+    = StackZipper 
+      (case p of
+         Add -> (case cs of
+                  (CInt x : CInt y : cs') -> CInt (x + y) : cs'
+                  _ -> error "bad use of addition")
+         Neg -> (case cs of 
+                  (CInt x : cs') -> CInt (-x) : cs'
+                  _ -> error "bad use of negation")
+         Not -> (case cs of 
+                  (CBool x : cs') -> CBool (not x) : cs'
+                  _ -> error "bad use of boolean not")
+         And -> (case cs of 
+                  (CBool x : CBool y : cs') -> CBool (x && y) : cs'
+                  _ -> error "bad use of boolean and")
+         Or -> (case cs of 
+                 (CBool x : CBool y : cs') -> CBool (x || y) : cs'
+                 _ -> error "bad use of boolean or")
+         Compose -> (case cs of
+                      (x : y : cs') 
+                        -> CRoutine ((case y of 
+                                        CRoutine o -> o
+                                        CPrimOp o  -> [DPrimOp o, DQuack]
+                                        _          -> error "bad use of composition") 
+                                  ++ (case x of
+                                        CRoutine o -> o
+                                        CPrimOp o  -> [DPrimOp o, DQuack]
+                                        _          -> error "bad use of composition")) 
+                         : cs'
+                      _ -> error "bad use of composition")
+         Pop -> (case cs of 
+                  (CStack (t : ts) : cs') -> t : CStack ts : cs'
+                  _ -> error "bad use of composition")
+         Push -> (case cs of
+                   (t : CStack ts : cs') -> CStack (t : ts) : cs'
+                   _ -> error "bad use of composition"))
+      ds
+
+cleanStackStep :: StackZipper -> StackZipper
+cleanStackStep s@(StackZipper cs []) = s
+cleanStackStep (StackZipper cs (DQuack : ds))
+    = case cs of
+        (CRoutine r : cs') -> StackZipper cs' (r ++ ds)
+        (CPrimOp p  : cs') -> applyPrimOp p $ StackZipper cs' ds
+        _                  -> error "quacked non-quackable value"
+cleanStackStep (StackZipper cs (d : ds))
+    = StackZipper 
+      (case d of
+        (DInt x)     -> CInt x : cs
+        (DChar x)    -> CChar x : cs
+        (DBool x)    -> CBool x : cs
+        (DRoutine x) -> CRoutine x : cs
+        (DPrimOp x)  -> CPrimOp x : cs
+        (DStack x)   -> CStack (cleanStack x) : cs)
+      ds
+
+cleanStack :: DirtyStack -> CleanStack
+cleanStack ds 
+    = generate
+      (StackZipper [] ds)
+      cleanStackStep
+      (\ (StackZipper _ xs) -> null xs)
+      (\ (StackZipper xs []) -> reverse xs)
   where
-    interpretExpression' :: EvaluationState -> Environment -> String
-    interpretExpression' es@(_, []) _ = showEvaluationState es
-    interpretExpression' es env       = interpretExpression' (stepEvaluationState es env) env
-
-showEvaluationState :: EvaluationState -> String
-showEvaluationState = undefined
-
-stepEvaluationState :: EvaluationState -> Environment -> EvaluationState
-stepEvaluationState = undefined
+    generate :: a -> (a -> a) -> (a -> Bool) -> (a -> b) -> b
+    generate init step done final
+        | done init = final init
+        | otherwise = generate (step init) step done final
 
--- README.md
+++ README.md
@@ -1,6 +1,108 @@
 # Quack-Stack
 
 Quack-Stack is a personal project that stemmed from my interest in different programming languages, especially esoteric languages.
-This language is my take on an esoteric programming language.
+This language is my take on an esoteric programming language what is stack based.
 
-Currently, the interpretter is being written in Haskell.
+Currently, the interpreter is being written in Haskell.
+
+## Features
+
+### AST Structure
+
+The AST for Quack-Stack consists of stacks, integers, characters, booleans, primitive operations, routines, and quacks. Each of these is called a term.
+
+Out of these terms, only quacks (represented with `!`) are not data, but they represent function evaluation.
+
+We can categorize terms into two distinct catgories: clean and dirty.
+
+Clean terms consist of:
+- stacks containing clean terms
+- integers
+- characters
+- booleans
+- routines
+- primitive operations
+
+Dirty terms are:
+- stacks containing dirty terms
+- integers
+- characters
+- booleans
+- routines
+- primitive operations
+- quacks
+
+Here we can now explain what each of the terms is.
+
+- A stack is a collection of terms with a front
+- A integer is an unbounded integer
+- A character is an ASCII character
+- A boolean is either True or False
+- A routine is a collection of dirty terms that are saved to evaluate later
+- A primitive operation changes the stack that preceeded it in a specified way when evaluated
+- A quack evaluates or applies the preceeding routine or primitive operation and the leftmost quack is the first applied
+
+Since routines and primitive operations interact very similarly in most situations, we refer to these as applicables.
+
+Every expression is a dirty stack.
+
+Additionally, there are a couple of different primitive operations which are implemented:
+- Add adds the previous two integer values in the stack
+- Neg negates the previous integer value in the stack
+- Not takes the boolean not of the previous boolean value in the stack
+- And takes the boolean and of the previous two boolean value in the stack
+- Or takes the boolean or of the previous two boolean value in the stack
+- Compose composes the previous two applicables in the stack into a new routine
+- Pop removes the top element of the preceeding substack and adds it to the stack
+- Push removes the previous element of the stack and adds it to the top of the preceeding substack
+
+### Evaluation Strategy
+
+Since eveery expression is just a dirty stack, all we have to do in order to evaluate the expression is clean the stack.
+
+Developing a stack cleaning strategy efficiently in Haskell relies on using a zipper. We keep removing from the bottom of the dirty stack, building up a clean stack until the dirty stack is empty. In this sense, we store the amount of evaluation that has been done so far, and the evaluation that is yet to be done together.
+
+It might be of interest to note that evaluating a routine will create some dirty terms again.
+
+Here is an example of this evaluation using a zipper.
+`()` represents a routine, and `[]` is a stack
+
+```
+step evaluation
+   [3 4 5 Neg ! Add Add Compose ! !]
+=> [3 4 -5 Add Add Compose ! !]
+=> [3 4 -5 (Add ! Add !) !]
+=> [3 4 -5 Add ! Add !]
+=> [3 -1 Add !]
+=> [2]
+
+step evaluation (with zipper)
+   clean                    dirty
+   []                       [3 4 5 Neg ! Add Add Compose ! !]
+=> [3]                      [4 5 Neg ! Add Add Compose ! !]
+=> [3 4]                    [5 Neg ! Add Add Compose ! !]
+=> [3 4 5]                  [Neg ! Add Add Compose ! !]
+=> [3 4 5 Neg]              [! Add Add Compose ! !]
+=> [3 4 -5]                 [Add Add Compose ! !]
+=> [3 4 -5 Add]             [Add Compose ! !]
+=> [3 4 -5 Add Add]         [Compose ! !]
+=> [3 4 -5 Add Add Compose] [! !]
+=> [3 4 -5 (Add ! Add !)]   [!]
+=> [3 4 -5]                 [Add ! Add !]
+=> [3 4 -5 Add]             [! Add !]
+=> [3 -1]                   [Add !]
+=> [3 -1 Add]               [!]
+=> [2]                      []
+```
+
+## Development Progress
+
+### Implemented
+
+- AST evaluation (Interpreter)
+
+### To Implement
+
+- Parser
+- Closures / Environments
+
